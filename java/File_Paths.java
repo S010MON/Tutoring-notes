539ded98a703e86c3687dc048aa8bca5d5ccb4e3
S010MON <leon@bdebnath.com>
2021-07-02 17:25:53 +0100

    "File paths added"

---
+++ java/File_Paths.java
@@ -0,0 +1,22 @@
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+
+public class File_Paths
+{
+	public static void main(String[] args)
+	{
+		// To read a file path from the system, you can use the File System library
+		
+		// This creates a new FileSystem object (remember to import both FileSystem and FileSystems!)
+		FileSystem fileSystem = FileSystems.getDefault();
+
+		// This prints out the path of the top level class being used
+        	System.out.println(fileSystem.getPath("").toAbsolutePath().toString());
+
+		// Below the top level class, we control the file structure, so we can now add our internal file path to the end
+		String topLevelPath = fileSystem.getPath("").toAbsolutePath().toString();
+		String internalPath = "your/file/path/here.txt";
+		String path = topLevelPath.concat(internalPath);
+		System.out.println("Your full path is: " + path);
+	}
+}
