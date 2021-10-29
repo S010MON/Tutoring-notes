c913557bf50142d86d3566e69cb60f5153bb3557
S010MON <leon@bdebnath.com>
2021-10-29 09:14:43 +0200

    Added interfaces for DSA

---
+++ java/DSA/MyLinkedListTest.java
@@ -0,0 +1,32 @@
+package DSA;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyLinkedListTest
+{
+    @Test void testAdd()
+    {
+
+    }
+
+    @Test void testGet()
+    {
+
+    }
+
+    @Test void testRemove()
+    {
+
+    }
+
+    @Test void testSize_0()
+    {
+
+    }
+
+    @Test void testSize_gt0()
+    {
+
+    }
+}
